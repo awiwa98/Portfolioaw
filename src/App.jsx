d06f92be3cfabaa0fea6091b223abc37b507eec0
awiwa98 <awiwawidmark123@icloud.com>
2025-10-20 21:53:15 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 import Home from './pages/Home';
 import About from './pages/About';
